5bbcf397e3b17388cf10b27e8d83435a37495474
Helen751 <okerekehelenugoeze@gmail.com>
2025-11-29 22:55:36 +0200

    web stack debugging 4 update

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,10 @@
-# Fix nginx under heavy load by increasing file descriptors and worker connections
+# Increase Nginx limits so it can handle heavy load (ab -c 100 -n 2000)
 
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/worker_connections 768;/worker_connections 4096;/" /etc/nginx/nginx.conf && service nginx restart',
+  command => '
+    sed -i "s/worker_processes .*/worker_processes 4;/" /etc/nginx/nginx.conf &&
+    sed -i "s/worker_connections .*/worker_connections 4096;/" /etc/nginx/nginx.conf &&
+    echo "ulimit -n 10000" >> /etc/default/nginx &&
+    service nginx restart
+  ',
 }
